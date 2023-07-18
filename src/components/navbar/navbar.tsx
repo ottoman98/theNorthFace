2c2be2325be375fc6230f3dbe247183906a7c078
ottoman98 <osmanzxc02@gmail.com>
2023-07-18 13:08:17 -0500

    siuu

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -88,6 +88,66 @@ function NavBar() {
                   </ul>
                 </div>
               </ul>
+
+              {/* mujer*/}
+
+              <ul className="categories-dropdown">
+                <li id="hombre">HOMBRE</li>
+
+                <div className="dropdown">
+                  <ul>
+                    <li>
+                      <b>PRODUCTOS DESTACADOS</b>
+                    </li>
+                    <li>NUEVA COLECCIÓN</li>
+                    <li>THERMOBALL</li>
+                    <li>VECTIV</li>
+                    <li>SALE & ÚLTIMAS UNIDADES</li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <b>CHAQUETAS Y CHALECOS</b>
+                    </li>
+                    <li>3 EN 1</li>
+                    <li>TÉRMICAS</li>
+                    <li>IMPERMEABLES</li>
+                    <li>POLAR</li>
+                    <li>ROMPEVIENTOS</li>
+                    <li>SOFTSHELLS</li>
+                    <li>CHALECOS</li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <b>ROPA</b>
+                    </li>
+                    <li>BUZOS</li>
+                    <li>CAMISAS Y POLOS</li>
+                    <li>CAMISETAS</li>
+                    <li>PANTALONES</li>
+                    <li>PANTALONETAS</li>
+                    <li>PRENDAS BÁSICAS</li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <b>ZAPATOS</b>
+                    </li>
+                    <li>BOTAS</li>
+                    <li>DE MONTAÑA & SENDERISMO</li>
+                    <li>DEPORTIVOS</li>
+                    <li>CASUALES</li>
+                    <li>CHANCLAS & SANDALIAS</li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <b>ACCESORIOS</b>
+                    </li>
+                    <li>GORRAS Y GORROS</li>
+                    <li>CUELLOS Y BUFANDAS</li>
+                    <li>GUANTES</li>
+                    <li>BOLSOS Y RIÑONERAS</li>
+                  </ul>
+                </div>
+              </ul>
             </ul>
           </li>
           <li className="nav-right">
